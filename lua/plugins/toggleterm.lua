39bb8c93ae0d390d02857edc64da52e230085166
Charlie <444coffey@gmail.com>
2026-09-15 11:29:15 -0400

    Enlarge vertical terminal width (very narrow previously)

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -22,7 +22,7 @@ return {
     },
     size = function(term)
       if term.direction == "vertical" then
-        return math.floor(vim.o.columns * 0.4)
+        return math.floor(vim.o.columns * 0.7)
       end
 
       return 20
